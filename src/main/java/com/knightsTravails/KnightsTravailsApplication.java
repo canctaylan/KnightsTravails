d8acc3fe07419ca8b53505e701e25f4a554932a6
cham_ <cham_@192.168.2.108>
2021-09-24 02:04:11 -0400

    Project creation

---
+++ src/main/java/com/knightsTravails/KnightsTravailsApplication.java
@@ -0,0 +1,13 @@
+package com.knightsTravails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightsTravailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnightsTravailsApplication.class, args);
+	}
+
+}
